ddba6f99bdd5d3189bc200048cd56035c60be8df
Roland Metivier <metivier.roland@chlorophyt.us>
2020-08-25 12:21:31 -0500

    add context functions

---
+++ include/celerytest_gl.hpp
@@ -0,0 +1,10 @@
+//
+// Created by accelshark on 8/25/20.
+//
+
+#ifndef CELERYTEST_CELERYTEST_SESSION_HPP
+#define CELERYTEST_CELERYTEST_SESSION_HPP
+
+class celerytest_session {};
+
+#endif // CELERYTEST_CELERYTEST_SESSION_HPP
---
+++ src/celerytest_gl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by accelshark on 8/25/20.
+//
+
+#include "celerytest_session.hpp"
